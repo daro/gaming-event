b8b4af7ac82080482e6768418112b43dcaebd9d9
Darek Jaroch <darek@jaroch.co.uk>
2023-11-15 09:23:58 +0000

    second init

--- public/js/plugins.js
+++ public/js/plugins.js
@@ -1,4 +1,4 @@
-// Avoid `console` errors in browsers that lack a console.
+
 (function() {
   var method;
   var noop = function () {};
@@ -10,6 +10,10 @@
   ];
   var length = methods.length;
   var console = (window.console = window.console || {});
+
+
+
+
 
   while (length--) {
     method = methods[length];
